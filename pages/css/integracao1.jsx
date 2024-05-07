f6a2b7c13c5fce4bc5e1bb5bb58fb3ce19b90687
Samilly <samillynunes19@gmail.com>
2024-05-07 20:07:44 -0300

    integrating with css: global

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,10 @@
+// O import do css foi feito na pagina _app.tsx de maneira global!
+export default function integration1(){
+    return (
+        <div>
+            <div className="vermelha">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <div className="branca">Texto #03</div>
+        </div>
+    );
+}
