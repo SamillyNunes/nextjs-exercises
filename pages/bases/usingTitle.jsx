710bda3d5bda1c16e9ca2b4dd44989677d889f4e
Samilly <samillynunes19@gmail.com>
2024-05-07 19:37:47 -0300

    using components

---
+++ pages/bases/usingTitle.jsx
@@ -0,0 +1,10 @@
+import Title from "../../components/Title";
+
+export default function usingTitle(){
+    return (
+        <div>
+            <h1>Usando Título:</h1><br/>
+            <Title/>
+        </div>
+    );
+}
